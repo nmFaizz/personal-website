3907325b206435c1e0925624c9a76b268552e08e
nmFaizz <ilznorz582@gmail.com>
2024-09-03 22:16:11 +0700

    feat: modify url config

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
     title: "Nur Muhammad Faiz",
     description: "I build web apps and write about tech-thoughts. Get To know me and my works",
-    url: "https://nmfaizz-rev3.vercel.app",
+    url: "https://nmfaizz.vercel.app",
 }
